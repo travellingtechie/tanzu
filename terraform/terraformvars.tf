c4651e4c84a5d00f3dae6b696e1610cd8af5351c
Brandon Neill <brandon@travellingtechie.com>
2022-09-14 23:01:51 -0600

    removed quotes around types in terraformvars.tf

--- terraform/terraformvars.tf
+++ terraform/terraformvars.tf
@@ -1,67 +1,67 @@
 # configure some variables first 
 variable "nsx" {
-    type = "map"
+    type = map
     description = "NSX Login Details"
 }
 variable "vsphere" {
-    type = "map"
+    type = map
     description = "vSphere Details"
 }
 variable "nsx_data_vars" {
-    type = "map"
+    type = map
     description = "Existing NSX vars for data sources"
 }
 variable "nsx_rs_vars" {
-    type = "map"
+    type = map
     description = "NSX vars for the resources"
 }
 variable "nsx_tag_scope" {
-    type = "string"
+    type = string
     description = "Scope for the tag that will be applied to all resources"
 }
 variable "nsx_tag" {
-    type = "string"
+    type = string
     description = "Tag, the value for the scope above"
 }
 variable "ipset" {
-    type = "list"
+    type = list
     description = "List of ip addresses that will be add in the IP-SET to allow communication to all VMs"
 }
 
 
 variable "app_listen_port" {
-    type = "string"
+    type = string
     description = "TCP Port the App server listens on"
 }
 
 variable "db_user" {
-    type = "string"
+    type = string
     description = "DB Details"
 }
 variable "db_pass" {
-    type = "string"
+    type = string
     description = "DB Details"
 }
 variable "db_name" {
-    type = "string"
+    type = string
     description = "DB Details"
 }
 
 variable "web" {
-    type = "map"
+    type = map
     description = "NSX vars for the resources"
 }
 variable "app" {
-    type = "map"
+    type = map
     description = "NSX vars for the resources"
 }
 variable "db" {
-    type = "map"
+    type = map
     description = "NSX vars for the resources"
 }
 
 variable "dns_server_list" {
-    type = "list"
+    type = list
     description = "DNS Servers"
 }
 
