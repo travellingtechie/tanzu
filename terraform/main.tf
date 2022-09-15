354f880dfd51184bd798f5636e42dd5c5b5bd6e5
Brandon Neill <brandon@travellingtechie.com>
2022-09-14 23:57:46 -0600

    added security group and service

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,8 +145,12 @@ resource "nsxt_policy_group" "tf-all" {
 resource "nsxt_policy_service" "app" {
   description       = "L4 Port range provisioned by Terraform"
   display_name      = "App Service"
-  protocol          = "TCP"
-  destination_ports = ["${var.app_listen_port}"]
+  l4_port_set_entry {
+    display_name      = "TCP${var.app_listen_port}"
+    description       = "TCP port ${var.app_listen_port} entry"
+    protocol          = "TCP"
+    destination_ports = ["${var.app_listen_port}"]
+  }
     tag {
 	scope = "${var.nsx_tag_scope}"
 	tag = "${var.nsx_tag}"
